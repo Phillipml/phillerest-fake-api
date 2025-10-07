30fa41d4a662ed5e039d06190e69a8f947e1913a
Phillipml <contato.phillip.menezes@gmail.com>
2025-10-06 21:11:22 -0300

    fix: corrigir imports de CSS e adicionar arquivos de teste para debug

---
+++ mocks/test.ts
@@ -0,0 +1,6 @@
+const testData = {
+  message: "Test data",
+  timestamp: new Date().toISOString()
+};
+
+export default testData;
---
+++ pages/api/test-import.ts
@@ -0,0 +1,10 @@
+import testData from '../../mocks/test'
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json(testData);
+}
